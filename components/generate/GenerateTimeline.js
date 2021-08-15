765dcaf3deee9680cb38d2be764626ff140c3340
MaxGies <Maxx_champ@hotmail.com>
2021-08-16 02:32:35 +0700

    Finish Firebase

--- components/generate/GenerateTimeline.js
+++ components/generate/GenerateTimeline.js
@@ -1,31 +1,86 @@
-import { useState } from "react";
-import classes from "./GenerateTimeline.module.css"
+import { useEffect, useState } from "react";
+import classes from "./GenerateTimeline.module.css";
+import { useCollection } from "react-firebase-hooks/firestore"
+import { firestore } from "../../utils/firebase";
 
-const DUMMY_INFO = {
-    "2020-08-13" : {
-        "10:30" : "ไปร้านขายข้าวแกงแล้วเจอแหวนทองแดงก็เลยเก็บไว้ซื้อกับข้าว",
-        "16:30" : "ไปซื้อชาย4หมี่เกี๊ยวแล้วร้องไห้เพราะเป็นเฟมินิส"
-    },
-    "2020-08-14" : {
-        "08:30" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia dui vel rhoncus tempus. Integer eget ante elit. Mauris pretium lorem et nisl rutrum, in mattis lorem dignissim. Nulla mi lectus, faucibus nec ultricies vel, ornare in ligula. Maecenas eu vulputate orci, in luctus odio. Proin cursus ligula nisl, dictum bibendum odio luctus ac. Donec pulvinar nunc dui, ac tempor est luctus id. Vivamus elementum rhoncus elit a mollis. Pellentesque ullamcorper dui sit amet gravida vulputate. Ut sit amet aliquet quam, et sollicitudin sapien.",
-        "12:30" : "ไปออกกำลังกายยามเที่ยง แดดร้อนๆ วิ่งได้ฟิลดี",
-        "18:30" : "ซื้อเฉาก๋วยชากังลาวมาเพราะแดดร้อนๆก็ต้องเฉาก๋วยชากังลาว"
-    },
-    "2020-08-15" : {
-        "10:30" : "ไปร้านขายข้าวแกงแล้วเจอแหวนทองแดงก็เลยเก็บไว้ซื้อกับข้าวอร่อยๆร้อนๆไฟลนปากแต่ก็บอกว่าแรงแรงอีก",
-        "16:30" : "ไปซื้อชาย4หมี่เกี๊ยวแล้วร้องไห้เพราะเป็นเฟมินิส"
-    },
-}
-
-const GenerateTimeline = (props) => {
+const GenerateTimeline = () => {
+    const [ user, setUser ] = useState([]);
+    const [ timeline, setTimeline] = useState({});
     const [ userAge, setUserAge ] = useState(0)
     const [ userGender, setUserGender ] = useState("ชาย")
     const [ userJob, setUserJob ] = useState("")
     const [ userDate, setUserDate ] = useState("")
     const [ userDescription, setUserDescription ] = useState("")
+    const sortObject = obj => Object.keys(obj).sort().reduce((res, key) => (res[key] = obj[key], res), {});
 
-    const userSubmitHandler = () => {
-        console.log(props.userInfo)
+    //recieve database isers info
+    const [dataUser, dataUserLoading, error] = useCollection(
+        firestore.collection('users'),
+        {}
+    );
+
+    //set recieve database to user
+    useEffect(()=>{
+        if(!dataUserLoading && dataUser){
+            const getUserDataFromDB = dataUser.docs.map( (doc) => ({
+            id: doc.id,...doc.data(),timeline: doc.data().timeline || {}
+            }))
+
+            setTimeline(getUserDataFromDB[0].timeline)
+            setUser(getUserDataFromDB[0]);
+            setUserAge(getUserDataFromDB[0].age)
+            setUserJob(getUserDataFromDB[0].job)
+            setUserGender(getUserDataFromDB[0].gender)
+        }
+    },[dataUser])
+
+    //click to deploy to database
+    const userSubmitHandler = async () => {
+        let dateTime = userDate.split('T')
+        
+        if(user.timeline != undefined){
+            if(Object.keys(user.timeline).length > 0 ){
+
+                if(user.timeline[dateTime[0]] != undefined){
+                    user.timeline[dateTime[0]][dateTime[1]] = userDescription
+                }
+
+                await setTimeline(Object.assign({ [dateTime[0]] : { [dateTime[1]] : userDescription },}, user.timeline));
+                await sendDataFireStore(Object.assign({ [dateTime[0]] : { [dateTime[1]] : userDescription },}, user.timeline));
+                
+            }
+            else{
+                await setTimeline(Object.assign({ [dateTime[0]] : { [dateTime[1]] : userDescription }, }))
+                await sendDataFireStore(Object.assign({ [dateTime[0]] : { [dateTime[1]] : userDescription }, }));
+            }
+        }
+
+        document.getElementById('description').value = ''
+    }
+
+    const sendDataFireStore = async (dataToFireStore) => {
+        await firestore.collection("users").doc(user.id).set({
+            age: userAge,
+            gender: userGender,
+            job: userJob,
+            timeline : dataToFireStore
+        })
+        .catch(error => {throw new Error('Error: Getting document:');});
+    }
+
+    const deleteInfoFromTimeline = async (date,time) => {
+
+        delete user.timeline[date][time];
+        
+        Object.keys(user.timeline).forEach(key => Object.keys(user.timeline[key]).length == 0 ? delete user.timeline[key] : 1  );
+
+        await firestore.collection("users").doc(user.id).set({
+            age: userAge,
+            gender: userGender,
+            job: userJob,
+            timeline : user.timeline
+            ,
+        });
     }
 
     return(
@@ -38,18 +93,18 @@ const GenerateTimeline = (props) => {
                         <div className={classes.inputFieldUser}>
                             <div>
                                 <label className={classes.textInfo} >เพศ</label>
-                                <select id="gender" name="gender" className={classes.selecGender} onChange={e => setUserGender(e.target.value)} >
+                                <select id="gender" name="gender" defaultValue={user.gender} className={classes.selecGender} onChange={e => setUserGender(e.target.value)} >
                                 <option>ชาย</option>
                                 <option>หญิง</option>
                                 </select>
                             </div>
                             <div>
                                 <label className={classes.textInfo} >อายุ</label>
-                                <input type="number" id="age" name="age" className={classes.ageInput} onChange={e => setUserAge(e.target.value)} />
+                                <input type="number" id="age" name="age" defaultValue={user.age} className={classes.ageInput} onChange={e => setUserAge(e.target.value)} />
                             </div>
                             <div className={classes.spawn2}>
                                 <label className={classes.textInfo} >อาชีพ</label>
-                                <input type="text" id="job" name="job" className={classes.ageInput} onChange={e => setUserJob(e.target.value)} />
+                                <input type="text" id="job" name="job" defaultValue={user.job} className={classes.ageInput} onChange={e => setUserJob(e.target.value)} />
                             </div>
                         </div>
                     </div>
@@ -77,24 +132,24 @@ const GenerateTimeline = (props) => {
                         <h3 className={classes.timelineUserInfo}>{`ผู้ป่วย${userGender} อายุ ${ userAge} ปี`}</h3>
                         <p className={classes.timelineUserInfoJob}>{`อาชีพ ${userJob}`}</p>
                     </div>
-                    {DUMMY_INFO?
+                    {timeline != undefined ? Object.keys(timeline).length > 0 ?
                         <div className={classes.timeline}>
-                            {Object.entries(DUMMY_INFO).map((info, index)=>{
+                            {Object.entries(sortObject(timeline)).map((infoDate, index)=>{
                                 return(
                                     <div key={index} className={classes.entireTimeline}>
                                         <div className={classes.titleTimeline}>
-                                            {info[0]}
+                                            {infoDate[0]}
                                         </div>
                                         <div className={classes.contentTimeline}>
                                         {
-                                            Object.entries(info[1]).map((info, index)=>{
+                                            Object.entries(sortObject(infoDate[1])).map((infoTime, index)=>{
                                                 return(
                                                     <div key={index} className={classes.contentTimeTelling}>
                                                         <p>
-                                                            <span className={classes.timeTelling}>{info[0]}</span>
-                                                            {info[1]}
+                                                            <span className={classes.timeTelling}>{infoTime[0]}</span>
+                                                            {infoTime[1]}
                                                         </p>
-                                                        <button className={classes.deleteTimeline}>x</button>
+                                                        <button className={classes.deleteTimeline} onClick={() => deleteInfoFromTimeline(infoDate[0],infoTime[0])}>x</button>
                                                     </div>
                                                 )
                                             })
@@ -105,7 +160,9 @@ const GenerateTimeline = (props) => {
                             }) }
                         </div>
                         :
-                        <h1>กรุณากรอกข้อมูลของท่าน</h1>
+                        <h1 className={classes.insertPlz}>กรุณากรอกข้อมูลของท่านเข้าสู่ระบบ</h1> 
+                        : 
+                        <h1 className={classes.insertPlz}>กรุณากรอกข้อมูลของท่านเข้าสู่ระบบ</h1>
                     }
                 </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,7 @@
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import { useCollection } from "react-firebase-hooks/firestore"
 import GenerateTimeline from '../components/generate/GenerateTimeline';
-import { firestore } from "../utils/firebase"
 
 const Home = () => {
-  const [user, setUser] = useState([]);
-  const [ inputUserData, setInputUserData ] = useState({});
-
-  const [dataUser, dataUserLoading, error] = useCollection(
-    firestore.collection('users'),
-    {}
-  );
-
-  useEffect(()=>{
-    if(!dataUserLoading && dataUser){
-      setUser( dataUser.docs.map( (doc) => ({
-        id: doc.id,...doc.data()
-      })))
-    }
-
-    console.log(user)
-  },[dataUserLoading])
-
-  const addUserHandler = async () => {
-    console.log(user)
-
-    // setInputUserData({})
-
-    // await firestore.collection("users").doc(user[0].id).set({
-    //   user
-    //   ,
-    // })
-
-    await firestore.collection("users").doc(user[1].id).set({
-      inputUserData
-      ,
-    })
-  }
-
   return (
     <div >
       <Head>
@@ -46,7 +9,7 @@ const Home = () => {
         <meta name="description" content="Covid Timeline Generator make you easy to generate your timeline" />
       </Head>
 
-      <GenerateTimeline userInfo={user[1]} />
+      <GenerateTimeline />
       
     </div>
   )
